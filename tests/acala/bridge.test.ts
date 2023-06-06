572ae48df8f7486f5639eee1c87ff2e6f5f340fe
joey <wind.zhaoyi@gmail.com>
2023-06-07 01:10:39 +0800

    update

--- tests/acala/bridge.test.ts
+++ tests/acala/bridge.test.ts
@@ -1,17 +1,18 @@
 import { Bridge } from '@polkawallet/bridge'
-import { FixedPointNumber } from '@acala-network/sdk-core';
-import { KaruraAdapter } from '@polkawallet/bridge/adapters/acala';
+import { FixedPointNumber } from '@acala-network/sdk-core'
+import { KaruraAdapter } from '@polkawallet/bridge/adapters/acala'
 import { KusamaAdapter } from '@polkawallet/bridge/adapters/polkadot'
 import { Network, createNetworks } from '../../networks'
 import { beforeEach, describe, it } from 'vitest'
+import { check, testingPairs } from '@acala-network/chopsticks-testing'
 import { checkEvents, checkSystemEvents, checkUmp } from '../../helpers'
-import { testingPairs } from '@acala-network/chopsticks-testing'
+import { queryTokenBalance } from '../../helpers/api/query'
 
 describe.each([
   {
     name: 'karura',
     relay: 'kusama'
-  },
+  }
 ] as const)('$name bridgeSDK', async ({ name, relay }) => {
   let relaychain: Network
   let parachain: Network
@@ -22,7 +23,7 @@ describe.each([
     beforeEach(async () => {
       const { [name]: parachain1, [relay]: relaychain1 } = await createNetworks({
         [name]: undefined,
-        [relay]: undefined,
+        [relay]: undefined
       })
 
       relaychain = relaychain1
@@ -35,47 +36,47 @@ describe.each([
     })
 
     it('xTokens works', async () => {
-      const karua = new KaruraAdapter();
+      const karua = new KaruraAdapter()
       await karua.init(parachain.api as any)
       const kusama = new KusamaAdapter()
       await kusama.init(relaychain.api as any)
-      const sdk = new Bridge({adapters: [karua, kusama]})
-      const fromAdapter = sdk.findAdapter(name);
+      const sdk = new Bridge({ adapters: [karua, kusama] })
+      const fromAdapter = sdk.findAdapter(name)
       const fromData = fromAdapter.getToken('KSM', fromAdapter.chain.id)
       const tx = await fromAdapter.createTx({
-        amount: FixedPointNumber.fromInner("10000000000", fromData.decimals),
+        amount: new FixedPointNumber(1, fromData.decimals),
         to: relay,
         token: 'KSM',
         address: alice.address,
-        signer: alice.address,
-      });
-
-      const event = tx.signAndSend(alice)
-      await parachain.chain.newBlock()
-      await checkEvents(event)
-      await checkUmp(parachain).redact({ number: true, hex: true }).toMatchSnapshot()
-
-      await relaychain.chain.newBlock()
-
-      await checkSystemEvents(relaychain, 'ump', 'staking').redact({ address: true, number: true }).toMatchSnapshot()
-    })
-  })
-
-  describe('releaychain bridge to parachain', () => {
-    beforeEach(async () => {
-      const { [name]: parachain1, [relay]: relaychain1 } = await createNetworks({
-        [name]: undefined,
-        [relay]: undefined,
+        signer: alice.address
       })
 
-      relaychain = relaychain1
-      parachain = parachain1
-
-      return async () => {
-        await relaychain.teardown()
-        await parachain.teardown()
-      }
+      const event = await tx.signAndSend(alice)
+      await parachain.chain.newBlock()
+      await checkEvents(event as any)
+      await checkUmp(parachain).redact({ number: true }).toMatchSnapshot()
+      await checkSystemEvents(relaychain).redact({address: true}).toMatchSnapshot()
+      await relaychain.chain.newBlock()
+      await check(queryTokenBalance(parachain.api, {Token: "KSM"}, alice.address)).toMatchSnapshot()
+      await check(relaychain.api.query.system.account(alice.address)).toMatchSnapshot()
     })
-
   })
+  //
+  // describe('releaychain bridge to parachain', () => {
+  //   beforeEach(async () => {
+  //     const { [name]: parachain1, [relay]: relaychain1 } = await createNetworks({
+  //       [name]: undefined,
+  //       [relay]: undefined,
+  //     })
+  //
+  //     relaychain = relaychain1
+  //     parachain = parachain1
+  //
+  //     return async () => {
+  //       await relaychain.teardown()
+  //       await parachain.teardown()
+  //     }
+  //   })
+  //
+  // })
 })
