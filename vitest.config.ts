b23ceb5874791b7b3885668fb34919cd7c69bf8c
Bryan Chen <xlchen1291@gmail.com>
2023-08-24 17:52:19 +1200

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
   test: {
     hookTimeout: 300000,
     testTimeout: 300000,
+    maxThreads: process.env.CI ? 1 : undefined,
+    minThreads: process.env.CI ? 1 : undefined,
   },
 })
