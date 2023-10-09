b56d25956deb2f8a644bfae9631487b3b6134226
Xiliang Chen <xlchen1291@gmail.com>
2023-10-09 17:08:15 +1300

    fix (#121)

--- networks/crust.ts
+++ networks/crust.ts
@@ -2,12 +2,12 @@ import { Config } from './types'
 
 export default {
   polkadot: {
-    name: 'crust' as const,
-    endpoint: 'wss://rpc-shadow.crust.network/',
-  },
-  kusama: {
     name: 'crustPolkadot' as const,
     endpoint: 'wss://crust-parachain.crustapps.net',
+  },
+  kusama: {
+    name: 'crust' as const,
+    endpoint: 'wss://rpc-shadow.crust.network/',
   },
   config: ({ alice }) => ({
     storages: {
