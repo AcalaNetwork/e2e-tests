d1a096b6d24620cb946ee87fc8b6827375e06f13
Ermal Kaleci <ermalkaleci@gmail.com>
2023-11-23 13:07:22 +0100

    update

--- networks/statemint.ts
+++ networks/statemint.ts
@@ -3,11 +3,11 @@ import { Config } from './types'
 export default {
   polkadot: {
     name: 'statemint' as const,
-    endpoint: 'wss://statemint-rpc.polkadot.io',
+    endpoint: 'wss://statemint-rpc.dwellir.com',
   },
   kusama: {
     name: 'statemine' as const,
-    endpoint: 'wss://kusama-asset-hub-rpc.polkadot.io',
+    endpoint: 'wss://statemine-rpc.dwellir.com',
   },
   config: ({ alice }) => ({
     storages: {
--- vitest.config.mts
+++ vitest.config.mts
@@ -6,7 +6,6 @@ export default defineConfig({
 	test: {
 		hookTimeout: process.env.CI ? minutes(2) : minutes(3),
 		testTimeout: process.env.CI ? minutes(5) : minutes(10),
-		maxThreads: 8,
-		minThreads: 1,
+		pool: 'forks',
 	},
 })
